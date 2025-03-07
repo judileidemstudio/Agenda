de80e99a638bf2778da5ae5c8a8167bbef8a07c5
judileidemstudio <judileide.mstudio@gmail.com>
2025-03-07 15:00:32 -0300

    Delete index.js

--- index.js
+++
@@ -1,7 +0,0 @@
-
-    document.querySelector('form').addEventListener('submit', function(event) {
-        event.preventDefault(); // Evita o envio do formulário
-        alert('Agendamento realizado com sucesso! Entraremos em contato para confirmar.');
-    });
-
-
